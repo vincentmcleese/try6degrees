a01cbb5b31bac7989f460a6707dafc1675f9f795
vincent <mcleesevj@gmail.com>
2024-08-28 14:24:09 +0200

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { NextUIProvider } from "@nextui-org/react";
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -23,7 +24,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <NextUIProvider>{children}</NextUIProvider>
+        <NextUIProvider>
+          {children} <Analytics />
+        </NextUIProvider>
       </body>
     </html>
   );
